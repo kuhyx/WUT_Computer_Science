e6273697672a5e2b0ee84346244e1df4f189c417
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2022-05-30 15:02:13 +0200

    merloj: Communication class base
    - also deleted unused Box Collider from Soldier prefab

--- theProject/Assets/Scripts/Entities/Communication.cs
+++ theProject/Assets/Scripts/Entities/Communication.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class Communication : MonoBehaviour
 {
+    public struct CommunicationResult
+    {
+        public bool resetTTL;
+        public List<Soldier> enemiesSpotted;
+    }
 
+    private float viewRange = 5f;
+    private Soldier mySoldier = null;
+    private Entity.Team myTeam = Entity.Team.Ally;
+
+    // ---------- public methods
+
+    public void Initialize(float _viewRange, Soldier _mySoldier, Entity.Team _myTeam)
+    {
+        viewRange = _viewRange;
+        mySoldier = _mySoldier;
+        myTeam = _myTeam;
+    }
+
+    public CommunicationResult HandleCommuncation()
+    {
+        CommunicationResult result = new CommunicationResult();
+        result.resetTTL = false;
+        result.enemiesSpotted = new List<Soldier>();
+
+        // look for soldiers in vincinity
+
+        // send Keep Alive (myTeam) - reset TTL (myTTL)
+
+        // look fo enemies (enemyTeam) - add Enemies to enemiesSpotted (send List<Enemies>)
+
+        return result;
+    }
+
+    // ---------- private methods
+
+    private Soldier[] GetSoldiersInVincinity()
+    {
+        //TO DO - implement
+        Physics.SphereCastAll()
+
+        return null;
+    }
 }
