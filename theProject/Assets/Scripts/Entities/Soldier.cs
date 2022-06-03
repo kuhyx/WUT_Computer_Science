46ab64aa48cb6336e1131e6e746479b4a65ec563
Krzysztof Stefan Rudnicki <k.s.rudnicki@stud.elka.pw.edu.pl>
2022-06-03 14:02:20 +0200

    restore old path finding value

--- theProject/Assets/Scripts/Entities/Soldier.cs
+++ theProject/Assets/Scripts/Entities/Soldier.cs
@@ -30,7 +30,7 @@ public class Soldier : Entity
                 return; // cannot find path: do nothing (for now)
 			}
 
-            Vector2Int movementStepDestination = path[path.Count - 1];
+            Vector2Int movementStepDestination = path[0];
 
 
             if (!TilemapManager.MoveSoldierS(soldier.tileCoord.x, soldier.tileCoord.y, movementStepDestination.x, movementStepDestination.y))
