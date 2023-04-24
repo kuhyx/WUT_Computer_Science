dc5d24826d09dd2aa90592f6888720b7f64c97fa
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2023-04-25 00:26:05 +0200

    feat: report, make main.py conform to pep8

--- lab4/main.py
+++ lab4/main.py
@@ -1,40 +1,69 @@
+"""
+Program that predicts wine quality based on variant2.csv data
+"""
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.metrics import mean_squared_error, accuracy_score, f1_score
 
-filename = '/home/kuchy/EARIN/lab4/variant2.csv'
+wine_df = pd.read_csv("variant2.csv")
+wine_df.head()
+wine_df.describe()
+wine_df.info()
 
-# Load the dataset
-wine_data = pd.read_csv(filename)
 
-# Split into features and labels
-X = wine_data.drop("quality", axis=1)
-y = wine_data["quality"]
+X = wine_df.iloc[:, :-1].values
+y = wine_df.iloc[:, -1].values
 
-# Split into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
 
-# Linear Regression
-lin_reg = LinearRegression()
-lin_reg.fit(X_train, y_train)
-y_pred_lin = lin_reg.predict(X_test)
-lin_reg_rmse = mean_squared_error(y_test, y_pred_lin, squared=False)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-# Logistic Regression
-log_reg = LogisticRegression(multi_class='multinomial', solver='newton-cg')
-log_reg.fit(X_train, y_train)
-y_pred_log = log_reg.predict(X_test)
-log_reg_accuracy = accuracy_score(y_test, y_pred_log)
+y_pred = regressor.predict(X_test)
 
-# SVM
-svm = SVC()
-svm.fit(X_train, y_train)
-y_pred_svm = svm.predict(X_test)
-svm_accuracy = accuracy_score(y_test, y_pred_svm)
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
 
-# Compare performance
-print("Linear Regression RMSE:", lin_reg_rmse)
-print("Logistic Regression accuracy:", log_reg_accuracy)
-print("SVM accuracy:", svm_accuracy)
+y_pred = classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+y_pred_train = regressor.predict(X_train)
+
+train_mse = mean_squared_error(y_train, y_pred_train)
+print("Training MSE:", train_mse)
+
+train_r_squared = regressor.score(X_train, y_train)
+print("Training R^2:", train_r_squared)
+
+test_r_squared = regressor.score(X_test, y_test)
+print("Testing R^2:", test_r_squared)
+y_pred_train = classifier.predict(X_train)
+
+train_accuracy = accuracy_score(y_train, y_pred_train)
+print("Training Accuracy:", train_accuracy)
+
+train_f1_score = f1_score(y_train, y_pred_train, average="weighted")
+print("Training F1 Score:", train_f1_score)
+
+test_f1_score = f1_score(y_test, y_pred, average="weighted")
+print("Testing F1 Score:", test_f1_score)
+
+Data1 = sns.countplot(x="quality", data=wine_df)
+plt.draw()
+plt.waitforbuttonpress(0)
+plt.close()
+Data2 = sns.heatmap(wine_df.corr(), annot=True)
+plt.draw()
+plt.waitforbuttonpress(0)
+plt.close()
