24d2a5fb44217dd9157cc197c0e74b03683a5262
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-05-30 23:44:48 +0200

    feat: filled controllers for all models

--- monorepo/apps/backend/src/app/zamowienie/zamowienie.controller.ts
+++ monorepo/apps/backend/src/app/zamowienie/zamowienie.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+// src/zamowienie/zamowienie.controller.ts
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ZamowienieService } from './zamowienie.service';
+import { Zamowienie } from '@prisma/client';
 
 @Controller('zamowienie')
-export class ZamowienieController {}
+export class ZamowienieController {
+  constructor(private readonly zamowienieService: ZamowienieService) {}
+
+  @Get()
+  async findAll(): Promise<Zamowienie[]> {
+    return this.zamowienieService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Zamowienie | null> {
+    return this.zamowienieService.findOne(+id);
+  }
+
+  @Post()
+  async create(@Body() data: { status: string }): Promise<Zamowienie> {
+    return this.zamowienieService.create(data);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: Partial<Zamowienie>): Promise<Zamowienie> {
+    return this.zamowienieService.update(+id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Zamowienie> {
+    return this.zamowienieService.delete(+id);
+  }
+}
--- monorepo/apps/backend/src/app/znizka/znizka.controller.ts
+++ monorepo/apps/backend/src/app/znizka/znizka.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+// src/znizka/znizka.controller.ts
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ZnizkaService } from './znizka.service';
+import { Znizka } from '@prisma/client';
 
 @Controller('znizka')
-export class ZnizkaController {}
+export class ZnizkaController {
+  constructor(private readonly znizkaService: ZnizkaService) {}
+
+  @Get()
+  async findAll(): Promise<Znizka[]> {
+    return this.znizkaService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Znizka | null> {
+    return this.znizkaService.findOne(+id);
+  }
+
+  @Post()
+  async create(@Body() data: { kod: string; wartosc: number; czy_dostepna: boolean; restauracjaId: number }): Promise<Znizka> {
+    return this.znizkaService.create(data);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: Partial<Znizka>): Promise<Znizka> {
+    return this.znizkaService.update(+id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Znizka> {
+    return this.znizkaService.delete(+id);
+  }
+}
