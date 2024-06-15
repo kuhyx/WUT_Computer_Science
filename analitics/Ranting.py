b45dd6700541fb47ef6257c80613d152b586e7c2
Alkaratus <kubama3ur@wp.pl>
2024-06-15 19:47:16 +0200

    Klasy+funkcje do uzupełnienia kwerendami

---
+++ analitics/Ranting.py
@@ -0,0 +1,9 @@
+
+
+class Ranting:
+    def __init__(self, id, movie_id, user_id, ranting, time):
+        self.id=id
+        self.movie_id=movie_id
+        self.user_id=user_id
+        self.ranting=ranting
+        self.time=time
