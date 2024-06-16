1370cde9a08f9c9ff0694d9d4c2bfc768bf0d9d0
gzub04 <gzub04@users.noreply.github.com>
2024-06-16 18:40:51 +0200

    Dodanie https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  server: { https: true }, // Not needed for Vite 5+
+  plugins: [ mkcert() ]
+})
