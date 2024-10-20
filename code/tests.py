838d5ed56303a72eb00f5941ca2f82631cf078fb
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-10-20 18:32:12 +0200

    fix: removed adding "size" to self values

--- code/tests.py
+++ code/tests.py
@@ -15,22 +15,29 @@ def test_richardson_vs_cg(n):
     solution_cg, info = cg(A, b)
     
     if info == 0:  # SciPy CG converged
-        assert_scipy_converged(solution_richardson, solution_cg, tolerance)
+        assert_scipy_converged(solution_richardson, solution_cg, tolerance, A, b)
     else:  # SciPy CG did not converge
-        assert_scipy_not_converged(solution_richardson)
+        assert_scipy_not_converged(solution_richardson, A, b)
 
-def assert_scipy_converged(solution_richardson, solution_cg, tolerance):
+def assert_scipy_converged(solution_richardson, solution_cg, tolerance, A, b):
     if solution_richardson == "Richardson method for those values will NOT converge":
         print("Richardson did not converge, while SciPy did")
+        print("Matrix A:\n", A)
+        print("Vector b:\n", b)
         assert False, "Richardson did not converge, while SciPy did"
     else:
         difference = np.linalg.norm(solution_richardson - solution_cg)
         print(f"Difference between Richardson and CG solutions: {difference:.8f}")
+        if difference >= tolerance:
+            print("Matrix A:\n", A)
+            print("Vector b:\n", b)
         assert difference < tolerance, f"The solutions are different! Difference: {difference:.8f}"
 
-def assert_scipy_not_converged(solution_richardson):
+def assert_scipy_not_converged(solution_richardson, A, b):
     if solution_richardson == "Richardson method for those values will NOT converge":
         print("Richardson and SciPy did not converge")
     else:
         print("Richardson converged while SciPy did not:", solution_richardson)
+        print("Matrix A:\n", A)
+        print("Vector b:\n", b)
         assert False, "Richardson converged while SciPy did not"
