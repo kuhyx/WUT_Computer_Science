ee59f88e4c8b2f6d8be288a24065216b210f153c
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-10-27 20:27:56 +0100

    chore: metadrivee <hello_world>

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import gymnasium as gym
-env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
-   observation, reward, terminated, truncated, info = env.step(action)
+import metadrive
 
-   if terminated or truncated:
-      observation, info = env.reset()
+# Initialize the MetaDrive environment
+env = metadrive.MetaDriveEnv()
 
+# Reset the environment to start a new episode
+env.reset()
+
+# Run the environment for a few steps
+for _ in range(10):
+    env.step(env.action_space.sample())  # Take random actions
+
+print("Hello, MetaDrive World!")  # Print a greeting
+
+# Close the environment after use
 env.close()
