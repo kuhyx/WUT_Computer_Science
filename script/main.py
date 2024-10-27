954ef7dbf2d3df5fd1de5228899daf6b20cd481c
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-10-27 21:17:59 +0100

    feat: made metadrive use gym insstead of gymnasium

--- script/main.py
+++ script/main.py
@@ -1,10 +1,11 @@
 from metadrive.envs.metadrive_env import MetaDriveEnv
 import gymnasium as gym
+from metadrive.envs.gym_wrapper import createGymWrapper # import the wrapper
 
-env = MetaDriveEnv(config={"use_render": True})
-obs, info = env.reset()
+env = createGymWrapper(MetaDriveEnv)(config={"use_render": True}) # wrap the environment
+obs = env.reset()
 for i in range(1000):
-    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    if terminated or truncated:
+    obs, reward, done, info = env.step(env.action_space.sample()) # the return value contains no truncate
+    if done:
         env.reset()
 env.close()
