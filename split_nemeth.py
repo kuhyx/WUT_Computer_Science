9d49ea894f3cf119aa978ed4d2b9fe80edd1f0c9
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-11-28 18:10:18 +0100

    feat: split poli not nemeth

--- split_nemeth.py
+++ split_nemeth.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Load the .npz file
-input_file = 'code/nemeth12.npz'
+input_file = 'code/poli3.npz'
 data = np.load(input_file)
 
 # Create dictionaries to hold the split data
@@ -11,7 +11,7 @@ first_half = {key: data[key] for key in data_keys[:split_point]}
 second_half = {key: data[key] for key in data_keys[split_point:]}
 
 # Save the halves to separate .npz files
-np.savez('nemeth12_1.npz', **first_half)
-np.savez('nemeth12_2.npz', **second_half)
+np.savez('poli3_1.npz', **first_half)
+np.savez('poli3_2.npz', **second_half)
 
-print(f"Data split into 'nemeth12_1.npz' and 'nemeth12_2.npz'")
+print(f"Data split into 'poli3_1.npz' and 'poli3_2.npz'")
